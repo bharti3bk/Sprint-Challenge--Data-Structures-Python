336c9b6b7392dfe4b8ed0e0d6d118a365bb0bd93
bharti3bk <bharti3bk@gmail.com>
2020-02-28 14:01:15 -0800

    Modified Ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,27 +1,40 @@
 from doubly_linked_list import DoublyLinkedList
 
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current_oldest = None
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        # add the value to dll
-        if self.storage.length < self.capacity:
-            # add to tail
-            self.storage.add_to_tail(item)
-
-        elif self.storage.length == self.capacity:
-
-            self.storage.add_to_tail(item)
+       
+         # If we reach the capacity of the buffer set the current pointer back to the beginning
+        if self.current_oldest is None:
+            self.current_oldest = self.storage.head
+        #Is there anything in our list yet? If not add the first item and 
+        #capacity full case
+        if self.storage.length == self.capacity and self.current_oldest == self.storage.head: #yes?
             self.storage.remove_from_head()
-
-
-    # returns all of the elements in the buffer in a list in their given order
-    # it should NOT return any None values
-    
+            self.storage.add_to_head(item)
+            self.current_oldest = self.current_oldest.next
+                 # set value
+                 #move current_oldest
+        elif self.storage.length == self.capacity and self.current_oldest == self.storage.tail:
+            self.storage.remove_from_tail()
+            self.storage.add_to_tail(item)
+            self.current_oldest = self.storage.head
+                 #set value
+                #set current oldnest 
+                #capacity not full case
+        #insert item and intialize current oldest value
+        elif self.storage.length >= self.capacity:
+            self.current_oldest.insert_before(item)
+            self.storage.length += 1
+            temp = self.current_oldest.next
+            self.storage.delete(self.current_oldest)
+            self.current_oldest = temp
+        else: #No?
+                self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -43,12 +56,12 @@ class RingBuffer:
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
